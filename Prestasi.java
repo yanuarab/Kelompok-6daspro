c5ae415b051b944f04e02af30818b1b7d835d951
JustinLaiya <laiyajustin@gmail.com>
2024-12-02 19:13:23 +0700

    PRESTASI MODIF

--- Prestasi.java
+++ Prestasi.java
@@ -9,7 +9,7 @@ public class Prestasi {
     public static void main(String[] args) {
         int pilihan;
         do {
-            System.out.println("\n=== MENU UTAMA ===");
+            System.out.println("\n= = = PRESTASI MAHASISWA = = =");
             System.out.println("1. Tambah Prestasi Mahasiswa");
             System.out.println("2. Tampilkan Semua Prestasi");
             System.out.println("3. Analisis Prestasi Berdasarkan Jenis");
@@ -29,7 +29,7 @@ public class Prestasi {
     }
 
     static void tampilkanMenu() {
-        System.out.println("\n=== MENU UTAMA ===");
+        System.out.println("\n= = = PRESTASI MAHASISWA = = =");
         System.out.println("1. Tambah Prestasi Mahasiswa");
         System.out.println("2. Tampilkan Semua Prestasi");
         System.out.println("3. Analisis Prestasi Berdasarkan Jenis");
@@ -40,10 +40,8 @@ public class Prestasi {
     static void tambahkanPrestasi() {
         System.out.print("Masukkan Nama Mahasiswa: ");
         prestasiList[jumlahPrestasi][0] = scanner.nextLine();
-
         System.out.print("Masukkan NIM Mahasiswa: ");
         prestasiList[jumlahPrestasi][1] = scanner.nextLine();
-
         System.out.print("Masukkan Jenis Prestasi: ");
         prestasiList[jumlahPrestasi][2] = scanner.nextLine();
 
@@ -59,7 +57,7 @@ public class Prestasi {
 
         int tahun;
         do {
-            System.out.print("Masukkan Tahun Prestasi (2010 hingga tahun saat ini): ");
+            System.out.print("Masukkan Tahun Prestasi (2010 hingga saat ini): ");
             tahun = scanner.nextInt();
             scanner.nextLine(); 
             if (tahun < 2010 || tahun > 2024) {
@@ -67,18 +65,17 @@ public class Prestasi {
             }
         } while (tahun < 2010 || tahun > 2024);
         tahunPrestasi[jumlahPrestasi] = tahun;
-
         jumlahPrestasi++;
-        System.out.println("Data prestasi berhasil ditambahkan.");
+        System.out.println("Data prestasi ditambahkan.");
     }
 
     static void menampilkanSemuaPrestasi() {
         if (jumlahPrestasi == 0) {
             System.out.println("Belum ada data prestasi.");
         } else {
-            System.out.println("\n=== DAFTAR PRESTASI MAHASISWA ===");
+            System.out.println("\n= = = DAFTAR PRESTASI MAHASISWA = = =");
             for (int i = 0; i < jumlahPrestasi; i++) {
-                System.out.printf("Nama: %s | NIM: %s | Jenis: %s | Tingkat: %s | Tahun: %d%n",
+                System.out.printf("Nama: %s | NIM: %s | Jenis Prestasi: %s | Tingkat: %s | Tahun: %d%n",
                         prestasiList[i][0], prestasiList[i][1], prestasiList[i][2], prestasiList[i][3], tahunPrestasi[i]);
             }
         }
@@ -89,7 +86,7 @@ public class Prestasi {
         String Analisis = scanner.nextLine();
         boolean ditemukan = false;
 
-        System.out.println("\n=== ANALISIS PRESTASI ===");
+        System.out.println("\n= = = ANALISIS PRESTASI = = =");
         for (int i = 0; i < jumlahPrestasi; i++) {
             if (prestasiList[i][2].equalsIgnoreCase(Analisis)) {
                 System.out.printf("Nama: %s | NIM: %s | Tingkat: %s | Tahun: %d%n",
